17c6fc0aa420cf9ee3ec455cb6e4bf84de103b93
vhavhugo <vhavhugo@hotmail.com>
2023-11-16 23:07:43 -0300

    Convenções - no python não existe palavras reservadas(public, private e protect)

---
+++ convencoes.py
@@ -0,0 +1,11 @@
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_person(self,):
+        return "<Person (%s, %s)>" % (self.name, self.age)
+    
+
+p = Person("John", 32)
+print("Type of Object:", type(p), "Memory Address:", id(p))
